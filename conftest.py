f721f9674bcffed085311466f1ab7855dd78ef9b
ichistov <ichistov@eng.vsofts.com>
2020-02-13 13:15:04 +0300

    обновление фикстуры для ИЕ

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ def pytest_addoption(parser):
     """Parser for command line parameters"""
     parser.addoption('--url',
                      action='store',
-                     default='http://192.168.1.232/opencart/',
+                     default='http://127.0.0.1/opencart/',
                      help='Main link for Opencart')
     parser.addoption('--browser_name',
                      action='store',
@@ -21,23 +21,17 @@ def browser_driver(request):
     browser = request.config.getoption("--browser_name")
     if browser == 'ie':
         print('\nStart Internet Explorer browser for test...')
-        options = webdriver.IeOptions()
-        driver_options(options)
-        browser = webdriver.Ie(options=options)
-        browser.implicitly_wait(40)
+        browser = webdriver.Ie()
     elif browser == 'chrome':
         print('\nStart Chrome browser for test...')
         options = webdriver.ChromeOptions()
-        driver_options(options)
+        options.add_argument("headless")
         browser = webdriver.Chrome(options=options)
-        browser.implicitly_wait(40)
     elif browser == 'firefox':
         print('\nStart Firefox browser for test...')
         options = webdriver.FirefoxOptions()
         options.add_argument("-headless")
-        # driver_options(options)
         browser = webdriver.Firefox(options=options)
-        browser.implicitly_wait(40)
     yield browser
     print('\nClose browser...')
     browser.quit()
@@ -48,8 +42,5 @@ def get_url(request, browser_driver):
     """Fixture for get link from parameter"""
     url = request.config.getoption("--url")
     open_link = browser_driver.get(url)
+    browser_driver.implicitly_wait(40)
     return open_link
-
-
-def driver_options(options):
-    options.add_argument("headless")
