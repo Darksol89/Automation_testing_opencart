ce4f86a20b28fc101bebf0bf8d464aa707671c2f
Ivan_Chistov <Darksol89@gmail.com>
2020-02-13 23:25:33 +0300

    Selectors for search page

---
+++ pages/search_page.py
@@ -0,0 +1,10 @@
+"""Selectors for different elements in the Opencart search page """
+from selenium.webdriver.common.by import By
+
+
+class SearchPage:
+    INPUT_SEARCH = (By.CSS_SELECTOR, '#input-search')
+    CATEGORIES_LIST = (By.XPATH, "//select[@name='category_id']")
+    CHECKBOX_DESCRIPTION = (By.CSS_SELECTOR, '#description')
+    CHECKBOX_SUBCATEGORIES = (By.XPATH, "//input[@name='sub_category']")
+    SEARCH_BUTTON = (By.CSS_SELECTOR, 'input[type="button"]')
