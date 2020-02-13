7f8005c589244699b0ee3011ac383e78e32d3233
ichistov <ichistov@eng.vsofts.com>
2020-02-13 17:53:29 +0300

    Added new tests for main page

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,15 +1,24 @@
 """Tests for visible other web element in the Opencart main page"""
 from pages.main_page import Selectors
 
-def test_logo_link(browser_driver, get_url):
-    logo_link = browser_driver.find_element(*Selectors.main_logo_link)
-    assert logo_link.is_displayed()
 
 def test_swiper_header(browser_driver, get_url):
     assert browser_driver.find_element(*Selectors.swiper_head)
 
+
 def test_swiper_footer(browser_driver, get_url):
     assert browser_driver.find_element(*Selectors.swiper_footer)
 
+
 def test_feature_header(browser_driver, get_url):
     assert browser_driver.find_element(*Selectors.featured_header)
+
+
+def test_button_group(browser_driver, get_url):
+    btn_group = browser_driver.find_elements(*Selectors.button_group)
+    assert len(btn_group) == 4
+
+
+def test_swiper_button(browser_driver, get_url):
+    swiper_btn = browser_driver.find_elements(*Selectors.swiper_buttons)
+    assert len(swiper_btn) == 2
