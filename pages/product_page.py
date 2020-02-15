91f0d985488869bb01ac9dd975256938ffd19323
Ivan_Chistov <Darksol89@gmail.com>
2020-02-15 20:44:52 +0300

    change some selectors

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,7 +1,9 @@
 """Selectors for different elements in the Opencart product page """
 from selenium.webdriver.common.by import By
 
+
 class ProductPage:
+    OPEN_PRODUCT_PAGE = (By.LINK_TEXT, 'MacBook')
     ADD_TO_CARD = (By.CSS_SELECTOR, '.form-group #button-cart')
     QUANTITY = (By.CSS_SELECTOR, '#input-quantity')
     ADD_TO_WISH_LIST = (By.XPATH, '//button[@data-original-title="Add to Wish List"]')
