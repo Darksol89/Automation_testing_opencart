d77f56adc24a273ed27f3a3d98aff8f0444489f4
Ivan_Chistov <Darksol89@gmail.com>
2020-02-15 20:46:13 +0300

    combine 5 small test to one test

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -2,27 +2,6 @@
 from pages.main_page import MainPage
 
 
-# def test_swiper_header(browser_driver, get_url):
-#     assert browser_driver.find_element(*MainPage.PROMOBLOCK_HEAD)
-#
-#
-# def test_swiper_footer(browser_driver, get_url):
-#     assert browser_driver.find_element(*MainPage.PROMOBLOCK_FOOTER)
-#
-#
-# def test_feature_header(browser_driver, get_url):
-#     assert browser_driver.find_element(*MainPage.PRODUCT_HEADER)
-#
-#
-# def test_button_group(browser_driver, get_url):
-#     btn_group = browser_driver.find_elements(*MainPage.BUTTON_GROUP)
-#     assert len(btn_group) == 4
-#
-#
-# def test_swiper_button(browser_driver, get_url):
-#     swiper_btn = browser_driver.find_elements(*MainPage.PROMOBLOCK_NAV_BUTTON)
-#     assert len(swiper_btn) == 2
-
 def test_main_page(browser_driver, get_url):
     browser_driver.find_element(*MainPage.PROMOBLOCK_HEAD)
     browser_driver.find_element(*MainPage.PROMOBLOCK_FOOTER)
