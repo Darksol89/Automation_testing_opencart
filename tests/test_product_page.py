46454c41d863cb1f217744c2d4765513368dc9c2
Ivan_Chistov <Darksol89@gmail.com>
2020-02-15 20:46:26 +0300

    added docstring

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,6 +1,9 @@
+"""Test different web elements in Product page"""
 from pages.product_page import ProductPage
 
+
 def test_product_page(browser_driver, get_url):
+    browser_driver.find_element(*ProductPage.OPEN_PRODUCT_PAGE).click()
     browser_driver.find_element(*ProductPage.ADD_TO_CARD)
     browser_driver.find_element(*ProductPage.ADD_TO_WISH_LIST)
     browser_driver.find_element(*ProductPage.COMPARE_PRODUCT)
