5e5d64b2f70f2cddd4f9ce58cc47e9d98715ae9a
Ivan_Chistov <Darksol89@gmail.com>
2020-02-16 20:06:25 +0300

    Added new selectors

--- pages/login_page.py
+++ pages/login_page.py
@@ -5,7 +5,10 @@ from selenium.webdriver.common.by import By
 class LoginPage:
     OPEN_LOGIN_PAGE = (By.XPATH, "//div[@class='col-sm-3']//a[text()='My Account']")
     EMAIL_INPUT = (By.CSS_SELECTOR, '#input-email')
+    USERNAME_INPUT = (By.CSS_SELECTOR, '#input-username')
     PASSWORD_INPUT = (By.CSS_SELECTOR, '#input-password')
     SUBMIT = (By.CSS_SELECTOR, 'input[type="submit"]')
+    LOGIN_BUTTON = (By.CSS_SELECTOR, 'button[type="submit"]')
     CONTINUE = (By.XPATH, '//*[contains(text(), "Continue")]')
     FORGOTTEN_PASSWORD = (By.LINK_TEXT, 'Forgotten Password')
+
