ce119224d3e3ca0c94f0636bf5bdb38f6db7fc7f
Ivan_Chistov <Darksol89@gmail.com>
2020-02-17 23:44:05 +0300

    selectors for Admin page

---
+++ pages/admin_dashboard_page.py
@@ -0,0 +1,15 @@
+"""Selectors for different elements in the Opencart admin page """
+from selenium.webdriver.common.by import By
+
+
+class AdminDashboard:
+    class Navigation:
+        CATALOG = (By.XPATH, '//li[@id="menu-catalog"]/a[contains(text(), "Catalog")]')
+        CATALOG_PRODUCTS = (By.XPATH, '//ul[@id="collapse1"]/li[2]/a[contains(text(), "Products")]')
+    class Products:
+        ADD_NEW = (By.CSS_SELECTOR, 'a[data-original-title="Add New"]')
+        EDIT = (By.CSS_SELECTOR, "a[data-original-title='Edit']")
+        REMOVE = (By.CSS_SELECTOR, "button[data-original-title='Delete']")
+        SAVE_NEW_PRODUCT = (By.CSS_SELECTOR, "button[data-original-title='Save']")
+        CANCEL_NEW_PRODUCT = (By.CSS_SELECTOR, "a[data-original-title='Cancel']")
+        PRODUCT_CHECKBOX = (By.CSS_SELECTOR, "input[type='checkbox']")
