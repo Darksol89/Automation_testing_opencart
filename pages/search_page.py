6ef7099aa073657e78e1928d59a70f591b09c467
Ivan_Chistov <Darksol89@gmail.com>
2020-03-01 11:57:59 +0300

    SearchPage переведена на  PageObject

--- pages/search_page.py
+++ pages/search_page.py
@@ -1,10 +1,31 @@
 """Selectors for different elements in the Opencart search page """
 from selenium.webdriver.common.by import By
+from pages.general_locators import GeneralSelectors
+from pages.BasePage import BasePage
 
 
-class SearchPage:
+class SearchPage(BasePage):
     INPUT_SEARCH = (By.CSS_SELECTOR, '#input-search')
     CATEGORIES_LIST = (By.XPATH, "//select[@name='category_id']")
     CHECKBOX_DESCRIPTION = (By.CSS_SELECTOR, '#description')
     CHECKBOX_SUBCATEGORIES = (By.XPATH, "//input[@name='sub_category']")
     SEARCH_BUTTON = (By.CSS_SELECTOR, 'input[type="button"]')
+
+    def open_search_page(self):
+        self.click_to_element(GeneralSelectors.OPEN_SEARCH_PAGE)
+
+    def input_search(self):
+        assert self.browser.find_element(*self.INPUT_SEARCH)
+
+    def categories_list(self):
+        assert self.browser.find_element(*self.CATEGORIES_LIST)
+
+    def checkbox_description(self):
+        assert self.browser.find_element(*self.CHECKBOX_DESCRIPTION)
+
+    def checkbox_subcategories(self):
+        assert self.browser.find_element(*self.CHECKBOX_SUBCATEGORIES)
+
+    def search_button(self):
+        search_btn = self.browser.find_element(*self.SEARCH_BUTTON)
+        assert search_btn.is_displayed()
