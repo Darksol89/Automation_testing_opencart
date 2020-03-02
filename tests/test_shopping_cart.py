88f5b95359e926e2c9ea50df87eacbea0f580756
ichistov <ichistov@eng.vsofts.com>
2020-03-02 16:35:26 +0300

    тесты на продуктовую корзину

---
+++ tests/test_shopping_cart.py
@@ -0,0 +1,31 @@
+"""Different tests for web elements in Cart page"""
+import pytest
+from pages import ProductPage, CartPage
+
+
+def test_update_product(browser_driver, get_url):
+    ProductPage(browser_driver)\
+        .open_product_page()\
+        .add_to_card()
+    CartPage(browser_driver)\
+        .open_cart()\
+        .click_view_cart()\
+        .click_update_product_in_cart()
+
+def test_remove_product(browser_driver, get_url):
+    ProductPage(browser_driver) \
+        .open_product_page() \
+        .add_to_card()
+    CartPage(browser_driver) \
+        .open_cart() \
+        .click_view_cart() \
+        .click_remove_product_from_cart()
+
+def test_continue_shopping(browser_driver, get_url):
+    ProductPage(browser_driver) \
+        .open_product_page() \
+        .add_to_card()
+    CartPage(browser_driver) \
+        .open_cart() \
+        .click_view_cart() \
+        .continue_shopping()
