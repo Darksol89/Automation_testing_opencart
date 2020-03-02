4ac92d4745265434758e7cc067dbb530d89ae739
ichistov <ichistov@eng.vsofts.com>
2020-03-02 16:33:03 +0300

    Переделан модуль под шаблон PageObject

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -10,52 +10,39 @@ class LoginPage(BasePage):
     EMAIL_INPUT = (By.CSS_SELECTOR, '#input-email')
     USERNAME_INPUT = (By.CSS_SELECTOR, '#input-username')
     PASSWORD_INPUT = (By.CSS_SELECTOR, '#input-password')
-    SUBMIT = (By.CSS_SELECTOR, 'input[type="submit"]')
+    LOGIN_SUBMIT = (By.CSS_SELECTOR, 'input[type="submit"]')
     LOGIN_BUTTON = (By.CSS_SELECTOR, 'button[type="submit"]')
     CONTINUE = (By.XPATH, "//div[@class='col-sm-6']//a[text()='Continue']")
     FORGOTTEN_PASSWORD = (By.LINK_TEXT, 'Forgotten Password')
 
     def open_account_page(self):
-        self.click_to_element(GeneralSelectors.OPEN_LOGIN_PAGE)
+        self._click_to_element(GeneralSelectors.OPEN_LOGIN_PAGE)
+        return self
 
-    def email_input(self, email):
-        try:
-            wait_for_element(self.browser, self.EMAIL_INPUT)
-            self.send_keys(email, self.EMAIL_INPUT)
-        except NoSuchElementException:
-            print('Email field is not available')
+    def login_user(self, email, password):
+        self._send_keys(email, self.EMAIL_INPUT)
+        self._send_keys(password, self.PASSWORD_INPUT)
+        self._click_to_element(self.LOGIN_SUBMIT)
+        return self
 
-        assert self.browser.find_element(*self.EMAIL_INPUT).get_attribute('value') == email
 
-    def username_input(self):
-        assert self.browser.find_element(*self.USERNAME_INPUT)
-
-    def password_input(self, password):
-        try:
-            wait_for_element(self.browser, self.PASSWORD_INPUT)
-            self.send_keys(password, self.PASSWORD_INPUT)
-        except NoSuchElementException:
-            print('Password field is not available')
-
-        assert self.browser.find_element(*self.PASSWORD_INPUT).is_displayed()
-
-    def continue_button(self):
+    def register_new_user(self):
         try:
             wait_for_element(self.browser, self.CONTINUE)
-            self.click_to_element(self.CONTINUE)
+            self._click_to_element(self.CONTINUE)
         except NoSuchElementException:
             print('Continue button is not available')
 
-        assert self.browser.title == 'Register Account'
+        return self
 
-    def forgotten_password(self):
+    def forgotten_password_form(self):
         try:
             wait_for_element(self.browser, self.FORGOTTEN_PASSWORD)
-            self.click_to_element(self.FORGOTTEN_PASSWORD)
+            self._click_to_element(self.FORGOTTEN_PASSWORD)
         except NoSuchElementException:
             print('Forgotten password button is not available')
 
-        assert self.browser.title == 'Forgot Your Password?'
+        return self
 
-    def login_button(self):
-        assert self.browser.find_element(*self.LOGIN_BUTTON).is_displayed()
+
+
