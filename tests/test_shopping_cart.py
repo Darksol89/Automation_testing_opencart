fd18930ecb4daf44e4683fc33d0ddce9f64d8409
ichistov <ichistov@eng.vsofts.com>
2020-03-03 19:46:42 +0300

    тесты корзины

--- tests/test_shopping_cart.py
+++ tests/test_shopping_cart.py
@@ -4,13 +4,14 @@ from pages import ProductPage, CartPage
 
 
 def test_update_product(browser_driver, get_url):
-    ProductPage(browser_driver)\
-        .open_product_page()\
+    ProductPage(browser_driver) \
+        .open_product_page() \
         .add_to_card()
-    CartPage(browser_driver)\
-        .open_cart()\
-        .click_view_cart()\
+    CartPage(browser_driver) \
+        .open_cart() \
+        .click_view_cart() \
         .click_update_product_in_cart()
+
 
 def test_remove_product(browser_driver, get_url):
     ProductPage(browser_driver) \
@@ -21,6 +22,7 @@ def test_remove_product(browser_driver, get_url):
         .click_view_cart() \
         .click_remove_product_from_cart()
 
+
 def test_continue_shopping(browser_driver, get_url):
     ProductPage(browser_driver) \
         .open_product_page() \
@@ -29,3 +31,5 @@ def test_continue_shopping(browser_driver, get_url):
         .open_cart() \
         .click_view_cart() \
         .continue_shopping()
+
+    assert browser_driver.browser.title == 'Your Store'
