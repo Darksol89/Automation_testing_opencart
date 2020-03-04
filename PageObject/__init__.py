7029fac324d4a6f6468bb577cf1940e242570f93
ichistov <ichistov@eng.vsofts.com>
2020-03-04 15:44:46 +0300

    Добавлены новые импорты

---
+++ PageObject/__init__.py
@@ -0,0 +1,6 @@
+from PageObject.CartPage import CartPage
+from PageObject.ProductPage import ProductPage
+from PageObject.LoginPage import LoginPage
+from PageObject.SearchPage import SearchPage
+from PageObject.MainPage import MainPage
+from PageObject.CatalogPage import CatalogPage
